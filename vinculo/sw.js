be7ed638b5ded7a98244d1b5083768ca42cc17e4
Fernando Zárate <fernando.fontanella90@gmail.com>
2026-09-22 13:46:18 -0300

    Vínculo: sin zoom, inputs 16px

--- vinculo/sw.js
+++ vinculo/sw.js
@@ -1,5 +1,5 @@
 // Vínculo · service worker (prototipo): cache-first para el shell, network-first para fuentes
-const CACHE = 'vinculo-v3';
+const CACHE = 'vinculo-v4';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
